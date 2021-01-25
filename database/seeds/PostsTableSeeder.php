9630c68bd6b64b3ec8dc4cf1edd00e3fe7ecfd3f
Alberto <thebertsinfo@gmail.com>
2021-01-25 14:02:17 +0100

    Utilizzo FakerPhP per popolare la tabella posts

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -10,9 +11,16 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         // $this->call(PostsTableSeeder::class);
-        
+        for ($i=0; $i < 50; $i++) {
+            $new_post_object = new Post();
+            $new_post_object->title = $faker->sentence(3);
+            $new_post_object->description = $faker->sentence(10);
+            $new_post_object->date_of_post = $faker->date();
+            $new_post_object->save();
+        }
+
     }
 }
