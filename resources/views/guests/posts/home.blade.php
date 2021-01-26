79c87139a655772092e9419e677e60dc88d3cb8b
Alberto <thebertsinfo@gmail.com>
2021-01-26 18:25:03 +0100

    Corretta visualizzazione posts per utenti normali

--- resources/views/guests/posts/home.blade.php
+++ resources/views/guests/posts/home.blade.php
@@ -8,27 +8,13 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>Titolo</th>
-                        <th>Descrizione</th>
-                        <th>Slug</th>
-                        <th>Data</th>
-                        <th>Azioni</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($posts as $post)
                         <tr>
-                            <td>{{ $post->id }}</td>
                             <td>{{ $post->title }}</td>
-                            <td>{{ $post->description }}</td>
-                            <td>{{ $post->slug }}</td>
-                            <td>{{ $post->date_of_post }}</td>
-                            <td>
-                                <a class="btn btn-info" href="#">
-                                    Visualizza
-                                </a>
-                            </td>
                         </tr>
                     @endforeach
                 </tbody>
