c24d918b98081dc19cb98bc5ca836503181ad77b
Alberto <thebertsinfo@gmail.com>
2021-02-01 16:26:52 +0100

    Validazione su view Edit

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -120,6 +120,13 @@ class PostController extends Controller
     public function update(Request $request, Post $post)
     {
         //
+        $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'category_id' => 'nullable|exists:categories,id',
+            'tags' => 'exists:tags,id'
+        ]);
+
         $form_data = $request->all();
 
         if($form_data['title'] != $post->title) {
@@ -139,7 +146,10 @@ class PostController extends Controller
             $form_data['slug'] = $slug;
         }
         $post->update($form_data);
-        $post->tags()->sync($form_data['tags']);
+        if(array_key_exists('tags', $form_data)) {
+            // aggiungo i tag al post
+            $post->tags()->sync($form_data['tags']);
+        }
         //Eseguo redirect su pagina Admin Home
         return redirect()->route('admin.posts.index');
     }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -10,27 +10,47 @@
                     Tutti i Post
                 </a>
             </div>
+            <div>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
             <form action="{{ route('admin.posts.update', ['post' => $post->id]) }}" method="post">
                 @csrf
                 @method('PUT')
                 <div class="form-group">
                     <label>Titolo</label>
-                    <input type="text" name="title" class="form-control" placeholder="Inserire Titolo..." value="{{ $post->title }}" maxlength="255" required>
+                    <input type="text" name="title" class="form-control" placeholder="Inserire Titolo..." value="{{ old('title', $post->title) }}" required>
+                    @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Descrizione</label>
-                    <textarea name="description" class="form-control" rows="10" placeholder="Inserire Descrizione..." required>{{ $post->description }}</textarea>
+                    <textarea name="description" class="form-control" rows="10" placeholder="Inserire Descrizione..." required>{{ old('content', $post->description)}}</textarea>
+                    @error('description')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label>Categoria</label>
                     <select class="form-control" name="category_id">
                         <option value="">-Seleziona Categoria-</option>
                         @foreach ($categories as $category)
-                            <option value="{{ $category->id }}" {{ $category->id == $post->category_id ? 'selected=selected' : '' }}>
+                            <option value="{{ $category->id }}" {{ $category->id == old('category_id', $post->category_id) ? 'selected=selected' : '' }}>
                                 {{ $category->name }}
                             </option>
                         @endforeach
                     </select>
+                    @error('category_id')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <p>Seleziona Tags: </p>
@@ -42,6 +62,9 @@
                             </label>
                         </div>
                     @endforeach
+                    @error('tags')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">
